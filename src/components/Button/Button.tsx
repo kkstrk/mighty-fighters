1b9ac57b115b62c44ca6281000a0a44036109b97
Kasia Kosturek <kasia.kosturek@gmail.com>
2025-10-05 21:19:31 -0400

    chore: update dependencies; test about dialog

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,11 @@
 import classes from "./Button.module.css";
 
-const Button = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-	return (
-		<button
-			className={classes.button}
-			type="button"
-			{...props}
-		>
-			{props.children}
-		</button>
-	);
-};
+const Button = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
+	<button
+		{...props}
+		className={classes.button}
+		type="button"
+	/>
+);
 
 export default Button;
